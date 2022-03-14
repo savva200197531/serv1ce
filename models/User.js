944c39b422a7339b7d5ce27b30eaab02ecbd17e4
savva200197531 <kashin.savva@mail.ru>
2022-03-14 23:53:52 +0700

    Add backend. Move directories.

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const { Schema, model, Types } = require('mongoose')
+
+const schema = new Schema({
+  email: {
+    type: String, required: true, unique: true
+  },
+  password: {
+    type: String, required: true
+  },
+  cart: [{
+    type: Types.ObjectId,
+    ref: 'Product'
+  }]
+
+})
+
+module.exports = model('User', schema)
